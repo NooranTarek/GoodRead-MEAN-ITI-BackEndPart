8148ec10553795b93a7629560ec2a819457f63a0
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-02-19 12:01:10 +0200

    add author functinality

---
+++ Controller/author.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-underscore-dangle */
+const Book = require('../models/book');
+const Author = require('../models/author');
+const CustomError = require('../lib/appError');
+
+const { paginationNum } = process.env;
+
+// 1-get authors
+
+const getAuthors = async (query) => { // /author?pageNum=1,popular=(true or false)
+  let authros;
+  // get authors pagination
+  if (!query.popular) {
+    authros = await Author.find().limit(paginationNum).skip((query.pageNum - 1) * paginationNum)
+      .exec()
+      .catch((err) => err);
+  }
+  // get popular authors that have the heighest num of books / apply pagination also
+  authros = await Book.aggregate([
+    {
+      $group: {
+        _id: '$author',
+        totalBooks: { $sum: 1 },
+      },
+    },
+    { $sort: { $totalBooks: -1 } },
+    { $skip: (query.pageNum - 1) * paginationNum },
+    { $limit: query.pageNum },
+  ])
+    .exec()
+    .catch((err) => err);
+  return authros;
+};
+
+// 2-create author
+
+const create = async (data) => {
+  // eslint-disable-next-line no-param-reassign
+  await Author.create(data)
+    .exec()
+    .catch((err) => {
+      throw new CustomError(err.message, 422);
+    });
+};
+
+// 3-update author
+
+const update = async (id, data) => {
+  await Author.findOneAndUpdate({ _id: id }, data)
+    .exec()
+    .catch((err) => {
+      throw new CustomError(err.message, 422);
+    });
+};
+
+// 4-delete author
+
+const deleteAthor = async (id) => {
+  await Author.deleteOne({ _id: id })
+    .exec()
+    .catch((err) => {
+      throw new CustomError(err.message, 422);
+    });
+};
+module.exports = {
+  getAuthors,
+  create,
+  update,
+  deleteAthor,
+};
