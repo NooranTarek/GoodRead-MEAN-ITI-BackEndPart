8b211e5fcb22347422ad675919524a8f7f6886e2
Nooran Tarek <noort.mohamed@gmail.com>
2024-02-21 01:31:32 +0200

    login user

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const { UserController } = require('../Controller');
 const asyncWrapper = require('../lib/asyncWrapper');
 const AppError = require('../lib/appError');
-// router.post('/register',UserController.register);
+
 router.post('/register', async (req, res, next) => {
   const [err, data] = await asyncWrapper(UserController.register(req.body));
-  console.log(err);
   if (!err) {
     res.json({ message: 'User created successfully', data });
     return;
@@ -14,4 +13,13 @@ router.post('/register', async (req, res, next) => {
   return next(new AppError(err.message, 400));
 });
 
+router.post('/login', async (req, res, next) => {
+  const [err, data] = await asyncWrapper(UserController.login(req.body));
+  if (!err) {
+    res.json({ message: 'User logged in successfully', data });
+    return;
+  }
+  // eslint-disable-next-line consistent-return
+  return next(new AppError(err.message, 400));
+});
 module.exports = router;
