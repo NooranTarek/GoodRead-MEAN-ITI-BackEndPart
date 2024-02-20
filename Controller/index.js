27f96915968ad2501e8e832bc09ee4ff6afc2b95
Nooran Tarek <noort.mohamed@gmail.com>
2024-02-21 01:50:13 +0200

    add admin and admin login

--- Controller/index.js
+++ Controller/index.js
@@ -3,5 +3,6 @@ module.exports = {
   AuthorController: require('./author'),
   BookController: require('./book'),
   UserController: require('./user'),
+  AdminController: require('./admin'),
   CategoryController: require('./categories'),
 };
