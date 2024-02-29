84723506a69ef604a71ac13fe1b6d4a0d73b9e33
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-02-29 19:24:43 +0200

    add multer image for admin

--- routes/admins.js
+++ routes/admins.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
+const multer = require('multer');
 const { AdminController } = require('../Controller');
 const asyncWrapper = require('../lib/asyncWrapper');
 const AppError = require('../lib/appError');
 const { isAuth } = require('../Middleware/authentication');
 const allowedTo = require('../Middleware/authorization');
 
-router.post('/', isAuth, allowedTo('admin'), async (req, res, next) => {
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'public/images/users');
+  },
+  filename(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+router.post('/', upload.single('image'), isAuth, allowedTo('admin'), async (req, res, next) => {
+  const imageOriginalName = req.file.originalname;
+  req.body.image = imageOriginalName;
   const [err, data] = await asyncWrapper(AdminController.addAdmin(req.body));
   if (!err) {
     res.json({ message: 'Admin created successfully', data });
