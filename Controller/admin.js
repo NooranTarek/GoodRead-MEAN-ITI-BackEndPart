611153f71e76c7e68a7329aaccceea39507d8362
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-02-29 19:31:51 +0200

    add updates to admin

--- Controller/admin.js
+++ Controller/admin.js
@@ -2,24 +2,20 @@ const JWT = require('jsonwebtoken');
 const AppError = require('../lib/appError');
 const { hashFunction, compareFunction } = require('../lib/hashAndCompare');
 const Users = require('../models/user');
-const Category = require('../models/category');
 
 // add admin
 const addAdmin = async (userData) => {
   const {
-    firstName, lastName, email, password, repassword, username, role,
+    firstName, lastName, email, password, username, image,
   } = userData;
-  if (password !== repassword) {
-    throw new AppError("Passwords don't match .. try to fill them again", 400);
-  } else {
-    const hashedPassword = await hashFunction({ plainText: password });
-    const newUser = await Users.create({
-      firstName, lastName, email, password: hashedPassword, username, role,
-    }).catch((err) => {
-      throw new AppError(err.message, 400);
-    });
-    return newUser;
-  }
+  const role = 'admin';
+  const hashedPassword = await hashFunction({ plainText: password });
+  const newUser = await Users.create({
+    firstName, lastName, email, password: hashedPassword, username, role, image,
+  }).catch((err) => {
+    throw new AppError(err.message, 400);
+  });
+  return newUser;
 };
 
 // login user
