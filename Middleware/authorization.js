5b2e27f9846768f1201146391d4e3d990e04788d
Nooran Tarek <noort.mohamed@gmail.com>
2024-03-01 11:45:52 +0200

    edits

--- Middleware/authorization.js
+++ Middleware/authorization.js
@@ -1,7 +1,7 @@
 const AppError = require('../lib/appError');
 
 const allowedTo = (...roles) => (req, res, next) => {
-  // console.log(req.user.role);
+  console.log(req.user)
   if (!roles.includes(req.user.role)) {
     throw new AppError(`you are not authorized you are ==> ${req.user.role}`, 403);
   } else {
